0bc0e6c8e8f595687d9f7915fa0ad82df69d9d87
Scott Dose <sdose27@gmail.com>
2022-04-12 20:15:31 -0700

    add script for Benchmark 1.1.20 Ensure noexec option set on removable media partitions

---
+++ 1_1_20_audit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Audit script to verify CIS Benchmark 1.1.20: Ensure noexec option set on removable media partitions
+source common.sh
+echo "Please manually verify that the noexec option is set on all removable media partitions"
+echo "run mount to see all mounted partitions"
+exit $EXIT_CODE_MANUAL
